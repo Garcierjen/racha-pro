ca17868112c10e42060a12b6b98a550ab8da2650
Garcierjen <heeyed036@gmail.com>
2026-06-09 06:21:39 -0400

    base_file

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { normalizeClass } from 'vue';
+</script>
+<template>
+  <body>
+    <div class="backgroundd"></div>
+    <div class="finder">
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+          <div class="aboutweb"></div>
+        </a>
+        <div class="webtitle">
+          Bangkokers
+          <div class="underline1"></div>
+          <div class="underline2"></div>
+          <div class="underline3"></div>
+        </div>
+    </div>
+    <div class="listofwhatido">
+      
+    </div>
+  <div class="on_scroll_pg">
+    <div class="footer_anchor">
+      <footer>
+        Made by Garcierjen on Github
+      </footer>
+      <a href="https://github.com/Garcierjen">
+          <div class="git_logo"></div>
+      </a>
+    </div>
+  </div>
+  </body>
+</template>
+<style scoped></style>
