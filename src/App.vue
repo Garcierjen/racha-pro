94f8ec865f903d3eedee4f8120527bd47467ad75
Garcierjen <heeyed036@gmail.com>
2026-06-10 10:52:44 -0400

    rework on page

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,29 @@ import { normalizeClass } from 'vue';
 </script>
 <template>
   <body>
-    <div class="backgroundd"></div>
-    <div class="finder">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-          <div class="aboutweb"></div>
-        </a>
-        <div class="webtitle">
-          Bangkokers
-          <div class="underline1"></div>
-          <div class="underline2"></div>
-          <div class="underline3"></div>
+    
+    <div class="on_scroll_pg">
+      <div class="mac-win-a">
+        <div class="mac-win-b">
+          <div class="mac-win-quit"></div>
+          <div class="mac-win-min"></div>
+          <div class="mac-win-full"></div>
+          <div class="mac-win-content">
+            <div class="mac-win-content-a">
+
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="footer_anchor">
+        <footer>
+          Made by Garcierjen on Github
+        </footer>
+        <a href="https://github.com/Garcierjen">
+            <div class="git_logo"></div>
+        </a>
+      </div>
     </div>
-    <div class="listofwhatido">
-      
-    </div>
-  <div class="on_scroll_pg">
-    <div class="footer_anchor">
-      <footer>
-        Made by Garcierjen on Github
-      </footer>
-      <a href="https://github.com/Garcierjen">
-          <div class="git_logo"></div>
-      </a>
-    </div>
-  </div>
   </body>
 </template>
 <style scoped></style>
