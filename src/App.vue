04a363eb28d5edf4ad817deec683965e387ed031
Garcierjen <heeyed036@gmail.com>
2026-06-11 10:22:45 -0400

    finder

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
+<!--
+note:
+    This is where page logic (front)
+    App.vue
+-->
+
 <script setup lang="ts">
-import { normalizeClass } from 'vue';
+import { normalizeClass, Suspense } from 'vue';
+import mainpg from './Basiclogin.vue';
 </script>
 <template>
-  <body>
-    
-    <div class="on_scroll_pg">
-      <div class="mac-win-a">
-        <div class="mac-win-b">
-          <div class="mac-win-quit"></div>
-          <div class="mac-win-min"></div>
-          <div class="mac-win-full"></div>
-          <div class="mac-win-content">
-            <div class="mac-win-content-a">
-
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="footer_anchor">
-        <footer>
-          Made by Garcierjen on Github
-        </footer>
-        <a href="https://github.com/Garcierjen">
-            <div class="git_logo"></div>
-        </a>
-      </div>
-    </div>
-  </body>
+  <mainpg/>
 </template>
-<style scoped></style>
---
+++ src/Basiclogin.vue
@@ -0,0 +1,36 @@
+<!--
+note:
+    This is where page live (front)
+    basiclogin.vue
+-->
+<template>
+  <body>
+    <div class="finder"> <!--Nav bar-->
+      
+    </div>
+    <div class="on_scroll_pg">
+      <div class="mac-win-a">
+        <div class="mac-win-b">
+          <div class="mac-win-quit"></div>
+          <div class="mac-win-min"></div>
+          <div class="mac-win-full"></div>
+          <div class="mac-win-content">
+            <label>Username</label>
+            <input type="text"></input>
+            <label>Password</label>
+            <input type="text"></input>
+          </div>
+          <div class="mac-win-content-a"></div>
+        </div>
+      </div>
+      <div class="footer_anchor">
+        <footer>
+          Made by Garcierjen on Github
+          <a href="https://github.com/Garcierjen">
+            <div class="git_logo"></div>
+        </a>
+        </footer>
+      </div>
+    </div>
+  </body>
+</template>
